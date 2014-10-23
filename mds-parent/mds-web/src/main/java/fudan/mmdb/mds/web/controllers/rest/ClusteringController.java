0f9b187aba32c9dc2b1db12c13c90061a682834e
JoshuaJiangFD <Joshua.Jiang@outlook.com>
2014-10-23 21:36:12 +0800

    f

--- mds-parent/mds-web/src/main/java/fudan/mmdb/mds/web/controllers/rest/ClusteringController.java
+++ mds-parent/mds-web/src/main/java/fudan/mmdb/mds/web/controllers/rest/ClusteringController.java
@@ -1,11 +1,16 @@
 package fudan.mmdb.mds.web.controllers.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.common.base.Strings;
 
 import fudan.mmdb.mds.web.model.ClusteredResponse;
 import fudan.mmdb.mds.web.model.SumRequest;
@@ -15,6 +20,9 @@ import fudan.mmdb.mds.web.services.RootService;
 @RequestMapping(value = { "/rest" })
 public class ClusteringController {
 
+	//slf4j: simple log facade library.
+	private static Logger LOGGER=LoggerFactory.getLogger(ClusteringController.class);
+	
 	@Autowired
 	private RootService rootService;
 
@@ -27,7 +35,11 @@ public class ClusteringController {
 	}
 
 	@RequestMapping(value = "/cluster/{queryTerm}", method = RequestMethod.GET, headers = { "Accept=application/json" })
-	public @ResponseBody ClusteredResponse clusteringViaQuery(String queryTerm) {
+	public @ResponseBody ClusteredResponse clusteringViaQuery(@PathVariable("queryTerm") String queryTerm) {
+		if(Strings.isNullOrEmpty(queryTerm)){
+			LOGGER.error("empty queryTerm is not allowed.");
+			return null;
+		}
 		return this.rootService.searchAndCluster(queryTerm);
 	}
 
